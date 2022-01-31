82a3df637aae38cd9d999b81338aad14174dff85
Bumler <bulmerhenry@gmail.com>
2022-01-30 21:44:53 -0500

    Build failure

--- src/test/kotlin/gameManagement/EnglishGuessValidatorTest.kt
+++ src/test/kotlin/gameManagement/EnglishGuessValidatorTest.kt
@@ -1,40 +1,46 @@
 package gameManagement
 
+import dataLayer.inMemoryRepos.InMemoryWordRepo
+import dataLayer.inMemoryRepos.InMemoryWordRepoDataProvider
 import org.junit.jupiter.api.Test
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class EnglishGuessValidatorTest {
     private val expectedLength = 5
-    private val englishGuessValidator: EnglishGuessValidator = EnglishGuessValidator(expectedLength)
+    //todo mock this
+    private val englishGuessValidator: EnglishGuessValidator = EnglishGuessValidator(expectedLength, InMemoryWordRepo(
+        InMemoryWordRepoDataProvider()
+    ))
+    private val previousGuesses = mutableSetOf<String>()
 
     @Test
     fun `invalid lengths are invalid`(){
         val testString = "shoe"
-        assertFalse { englishGuessValidator.validate(testString).isSuccess }
+        assertFalse { englishGuessValidator.validate(testString, previousGuesses).isSuccess }
     }
 
     @Test
     fun `can't have symbols`(){
         val testString = "shoe!"
-        assertFalse { englishGuessValidator.validate(testString).isSuccess }
+        assertFalse { englishGuessValidator.validate(testString, previousGuesses).isSuccess }
     }
 
     @Test
     fun `can't have numbers`(){
         val testString = "shoe1"
-        assertFalse { englishGuessValidator.validate(testString).isSuccess }
+        assertFalse { englishGuessValidator.validate(testString, previousGuesses).isSuccess }
     }
 
     @Test
     fun `definitely can't have umlauts`(){
         val testString = "schön"
-        assertFalse { englishGuessValidator.validate(testString).isSuccess }
+        assertFalse { englishGuessValidator.validate(testString, previousGuesses).isSuccess }
     }
 
     @Test
     fun `valid words are valid`(){
         val testString = "cacti"
-        assertTrue { englishGuessValidator.validate(testString).isSuccess }
+        assertTrue { englishGuessValidator.validate(testString, previousGuesses).isSuccess }
     }
 }
